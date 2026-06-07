0fc3f6423da62be265fce574a50881289219bb44
itsumogakusei <attilakaradeniz@gmail.com>
2026-06-08 01:08:19 +0200

    add UI theme colors

---
+++ app/src/main/java/com/example/androchess/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.androchess.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Application branding colors
+val PrimaryColor = Color(0xFF005588)
+val SecondaryColor = Color(0xFF00BCD4)
+val NeonBlue = Color(0xFF00FFFF)
+val DarkBackground = Color(0xFF121212)
